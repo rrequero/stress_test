a6feed74eb069fd3f7e41075e71dad3d63f56bee
Ra <rareq1987@gmail.com>
2015-10-27 11:45:54 +0100

    pruebas

--- koa_app/index.js
+++ koa_app/index.js
@@ -7,12 +7,24 @@ var app = koa();
 
 app.use(bodyParser());
 
+var users = [{
+            name:'Ra',
+            job:'Developer'
+          }, {
+            name: 'Pepe',
+            job: 'Backend Developer'
+          }, {
+            name: 'Juan',
+            job: 'Designer'
+          }];
 var router = new Router();
-router.get('/test/:id/:name', function * () {
-    this.body = {
-        id: this.params.id,
-        name: this.params.name
-    };
+router.get('/api/users', function * () {
+    this.body = users;
+});
+
+router.post('/api/users', function * () {
+    users.push(this.request.body);
+    this.body = this.request.body;
 });
 app.use(router.middleware());
 app.listen(3000);
